73ce9eb0aeb49b3e3628fbf4d58a052f54060fd8
Claudio Rodríguez Rogel <40474902+crodriguezrogel@users.noreply.github.com>
2025-07-13 17:49:43 -0400

    Add files via upload
    Mi primer programa en Python

---
+++ Modulo_02/hola.py
@@ -0,0 +1,10 @@
+# hola.py - tu primer programa en Python
+nombre = input("¿Cuál es tu nombre? ")
+print(f"¡Hola, {nombre}! Bienvenido al munndo de Python.")
+edad = int(input("¿Cuántos años tienes? "))
+if edad >= 18:
+    print("Eres mayor de edad.")
+else:
+    print("Eres menor de edad.")    
+    
+
